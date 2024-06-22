1c41407e9894439ed6f0e790c2ab6dafdd1ec6aa
PalomaSalinas19 <palomaiesv@gmail.com>
2024-06-22 10:52:20 +0200

    A

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/',(req, res) => res.send('<h1>Express con HTML</h1>'))
+app.get('/',(req, res) => res.send('<h1>Express con HTML!!!'))
 
 app.listen(8080);
 console.log('Server on port 8080')
